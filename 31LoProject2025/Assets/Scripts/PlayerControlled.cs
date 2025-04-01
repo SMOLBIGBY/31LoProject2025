d0453c4624765f73c750bab41fc68acb52a3bdf0
SMOLBIGBY <maksimka.sheff@gmail.com>
2025-04-01 16:52:44 +0200

    base1

---
+++ 31LoProject2025/Assets/Scripts/PlayerControlled.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    
+    public float Speed = 10;
+
+    public float JumpForce = 5;
+
+    public bool Grounded = false;
+
+    Rigidbody2D _rb;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ 31LoProject2025/Assets/Scripts/UpdateT.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class UpdateT : MonoBehaviour
+{
+
+    public float _playerHealth = 100;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
