08c6162c5a957fa631942db9df9492aee69c76a6
SMOLBIGBY <maksimka.sheff@gmail.com>
2025-04-07 17:24:32 +0200

    1234

---
+++ 31LoProject2025/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+public class PouseMenu : MonoBehaviour
+{
+    public static bool GameIsPaused = false; 
+    public GameObject PauseMenuUI; 
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        PauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    public void Pause()
+    {
+        PauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log ("quit");
+        Application.Quit();
+    }
+}
