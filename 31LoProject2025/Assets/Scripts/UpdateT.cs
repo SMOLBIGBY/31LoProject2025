82fd5d6c42f2f232e704a55cfb0fa888e3296b58
KiwiKaktuss <filip@solomka.me>
2025-04-10 16:29:12 +0200

    Healthbar

--- 31LoProject2025/Assets/Scripts/UpdateT.cs
+++ 31LoProject2025/Assets/Scripts/UpdateT.cs
@@ -1,16 +1,46 @@
 using UnityEngine;
-
+using UnityEngine.SceneManagement; 
+using UnityEngine.UI;
 public class UpdateT : MonoBehaviour
 {
+    [SerializeField] private int health = 100; 
     public bool _cutscene = false;
-
-    void Start()
+    private void Start()
     {
-
+        
+        health = PlayerPrefs.GetInt("playerHealth", 100);  
+        Debug.Log("Załadowane zdrowie: " + health);
     }
 
-    void Update()
+    private void OnApplicationQuit()
+    {
+        
+        SaveHealth();
+    }
+
+    private void OnDisable()
+    {
+       
+        SaveHealth();
+    }
+
+    public void SaveHealth()
     {
 
+        PlayerPrefs.SetInt("playerHealth", health);
+        PlayerPrefs.Save();  
+    }
+
+    public void SetHealth(int newHealth)
+    {
+        health = newHealth;
+    }
+
+    
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+        if (health < 0) health = 0;  
+        Debug.Log("Zaktualizowane zdrowie: " + health);
     }
 }
